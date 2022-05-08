a0e7c2cae177c00bb054343e00305928881b202b
NoobProgrammerRoy <roystonrodrigues24@gmail.com>
2022-05-08 21:37:41 +0530

    Changes

--- index.js
+++ index.js
@@ -3,11 +3,6 @@ const app = express();
 const apiRouter = require('./routes/apiRouter');
 const port = process.env.PORT || 3000;
 
-let myRouter = express.Router();
-myRouter.get('/', (req, res) => {
-    res.send('<h1>Hello World<h1>');
-})
-
 app.use('/api', apiRouter);
 
 app.listen(port, () => console.log(`Server is running on port: ${port}`));
